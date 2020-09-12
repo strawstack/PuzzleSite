fc309fdf229f080ddec5518d732d19ff208bd7de
Richard Hayes <rhshadowgreen@gmail.com>
2020-09-12 12:33:35 -0700

    Add build folder to checked in files

---
+++ build/precache-manifest.47e9b5727597694800d5186d6d14e4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec5d0c90a70a1c52fa9037e6b777ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d2560b3ed00af426cee",
+    "url": "/static/css/main.f5fc6c1c.chunk.css"
+  },
+  {
+    "revision": "ee6a75254f403b7548c4",
+    "url": "/static/js/2.c1eb09bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c1eb09bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2560b3ed00af426cee",
+    "url": "/static/js/main.bf161136.chunk.js"
+  },
+  {
+    "revision": "39ce663ce569126f5183",
+    "url": "/static/js/runtime-main.800cff1d.js"
+  },
+  {
+    "revision": "14d05fff1df33ea2cd536caa9b74b707",
+    "url": "/static/media/hex.14d05fff.jpeg"
+  }
+]);
